2d4d619bdb07078aeb012887c662f5916e88aa7f
Evian <icdk5b4zf@relay.firefox.com>
2022-09-23 10:00:52 +0200

    feat: add extra treatment step of raw logs

--- scripts/get_player_logs_csv.py
+++ scripts/get_player_logs_csv.py
@@ -1,23 +1,27 @@
 import os
 from tqdm import tqdm
 
-path = "logs_Mush2"
-NB_FOLDERS = 2
+import pandas as pd
 
-with open(os.getcwd() + "/" + "clean_player_logs.csv", "w") as f:
-    print("Ship|Character|Day.Cycle|Action|Log", file=f)
+path = "data/logs"
+NB_FOLDERS = 4
 
-for i in tqdm(range(1, NB_FOLDERS + 1)):
+with open("data" + "/" + "new_player_logs.csv", "w") as f:
+    print("Ship|Character|Day.Cycle|Event|Log", file=f)
+
+for i in tqdm(range(1711, NB_FOLDERS + 1711)):
     folder_name = path + "/" + str(i)
     #open the folder
     for filename in os.listdir(folder_name):
-        #open txt files
-        if filename.endswith(".txt") and not "Morts" in filename and not "NERON" in filename:
             #open the file
             with open(folder_name + "/" + filename, "r") as f:
                 for line in f:
                     if "[ADM]" in line or not "[" in line or "NERON" in line:
                         continue
-                    line +=  str(i) + "|" + filename.split(".")[0].split("-")[1] + "|"
-                    print(line, file=open(os.getcwd() + "/" + "test.csv", "a"), end='', sep='\n')
+                    line +=  str(i) + "|" + filename.split(".txt")[0] + "|"
+                    print(line, file=open("data" + "/" + "new_player_logs.csv", "a"), end='')
     
+new_logs = pd.read_csv("data/new_player_logs.csv", sep="|")
+new_logs = new_logs.drop([0, len(new_logs) - 1])
+new_logs["Event"] = new_logs["Event"].apply(lambda x: x.split("EV:")[-1].split("]")[0])
+new_logs.to_csv("data/new_player_logs.csv", index=False)
