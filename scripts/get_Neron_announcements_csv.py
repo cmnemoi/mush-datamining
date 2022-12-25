ba3735572ca281db069214a765db12904e97b078
Evian <icdk5b4zf@relay.firefox.com>
2022-12-26 00:42:08 +0100

    feat: improve the script to preprocess NERON announcements raw data

--- scripts/get_Neron_announcements_csv.py
+++ scripts/get_Neron_announcements_csv.py
@@ -1,16 +1,25 @@
 import os
+
+from glob import glob
 from tqdm import tqdm
 
-path = "logs_Mush2"
+import pandas as pd
+
+LOGS_FOLDER_PATH = "logs_Mush2"
+DESTINATION_PATH = "data/NERON_announcements.csv"
 NB_FOLDERS = 1710
 
-with open(os.getcwd() + "/" + "NERON_announcements.txt", "w") as f:
-    print("Day.Cycle|Annoucement\n", file=f, end='', sep='\n')
+with open(DESTINATION_PATH, "w") as f:
+    print("Ship|Day.Cycle|Annoucement\n", file=f, end='', sep='\n')
 
-for i in tqdm(range(1, NB_FOLDERS + 1)):
-    folder_name = path + "/" + str(i)
-    for filename in os.listdir(folder_name):
-        if filename.endswith(".txt") and "NERON" in filename:
-            with open(folder_name + "/" + filename, "r") as f:
-                for line in f:
-                    print(line.strip(), file=open(os.getcwd() + "/" + "NERON_announcements.txt", "a"))
+neron_files = glob(LOGS_FOLDER_PATH + "/**/*NERON.txt", recursive=True)
+
+for i, file_name in enumerate(tqdm(neron_files, total=len(neron_files))):
+    with open(file_name, "r") as file:
+        for line in file:
+            line += str(i) + "|" # add ship number
+            line = line.replace("||", "") # remove double pipes
+            print(line.strip(), file=open(DESTINATION_PATH, "a"), end='')
+
+# remove last line of file
+os.system("sed -i '$ d' " + DESTINATION_PATH)
