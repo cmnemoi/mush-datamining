f10d8210c88da4833a8f0be90fc53904521df25c
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-03-19 18:28:35 +0100

    Revert "setup Heroku deployment"
    This reverts commit 06bd0b1a2ee73a8c428dfb410a65bee4f8b0cffd.

--- setup.sh
+++
@@ -1,6 +0,0 @@
-
-mkdir -p ~/.streamlit/
-
-echo [general]nemail = "icdk5b4zf@relay.firefox.com"n > ~/.streamlit/credentials.toml
-
-echo [server]nheadless = truenenableCORS=falsenport = n > ~/.streamlit/config.toml
