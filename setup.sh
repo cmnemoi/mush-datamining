972a63aae77ec98af45dc69cddc68302b8e8de00
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-03-19 18:28:27 +0100

    Revert "update setup.sh"
    This reverts commit 729fbed18494d672eec2cf1c4ae836bc3e7f4059.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
+
 mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"icdk5b4zf@relay.firefox.com\"\n\
-" > ~/.streamlit/credentials.toml
+echo [general]nemail = "icdk5b4zf@relay.firefox.com"n > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo [server]nheadless = truenenableCORS=falsenport = n > ~/.streamlit/config.toml
