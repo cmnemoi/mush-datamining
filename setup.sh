06bd0b1a2ee73a8c428dfb410a65bee4f8b0cffd
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-03-19 17:38:59 +0100

    setup Heroku deployment

---
+++ setup.sh
@@ -0,0 +1,6 @@
+
+mkdir -p ~/.streamlit/
+
+echo [general]nemail = "icdk5b4zf@relay.firefox.com"n > ~/.streamlit/credentials.toml
+
+echo [server]nheadless = truenenableCORS=falsenport = n > ~/.streamlit/config.toml
